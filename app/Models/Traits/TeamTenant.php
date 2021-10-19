49241c242be6e585a1408b75a1f2a866f3853b8f
Vitor Dio <vitor.gracia@gmail.com>
2021-10-19 16:19:29 -0300

    Tenant, Policy para visualização dos contatos de acordo com o time do usuário

---
+++ app/Models/Traits/TeamTenant.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models\Traits;
+
+use App\Models\Scopes\BelongsToTeam;
+use App\Models\Team;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Auth;
+
+trait TeamTenant
+{
+    public static function bootTeamTenant(): void
+    {
+        static::creating(function (Model $model) {
+            if (! $model->team_id) {
+                $model->team_id = Auth::user()->currentTeam->id;
+            }
+        });
+
+        static::addGlobalScope(new BelongsToTeam());
+    }
+
+    public function team(): BelongsTo
+    {
+        return $this->belongsTo(Team::class);
+    }
+}
