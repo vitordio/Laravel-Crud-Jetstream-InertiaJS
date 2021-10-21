2585648c458f76b1cd58eaede355564e3d39c1f8
Vitor Dio <vitor.gracia@gmail.com>
2021-10-21 14:52:19 -0300

    Translete messages to pt-br

---
+++ resources/lang/pt-br/labels.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    // Usuários
+    'id' => 'ID',
+    'name' => 'Nome',
+    'email' => 'E-mail',
+    'password' => 'Senha',
+    'password_confirmation' => 'Confirmação da Senha',
+];
